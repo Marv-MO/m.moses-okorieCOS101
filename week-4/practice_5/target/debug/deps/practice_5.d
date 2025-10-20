c827cd356d5c75a1b87b203657dac0509003c52a
Marvelous Moses-Okorie <marvelous.moses-okorie@pau.edu.ng>
2025-10-20 14:54:32 +0100

    (My seventh commit)

---
+++ week-4/practice_5/target/debug/deps/practice_5.d
@@ -0,0 +1,5 @@
+C:\Users\marve\OneDrive\Documents\m.moses-okorieCOS101\week-4\practice_5\target\debug\deps\practice_5.d: src\main.rs
+
+C:\Users\marve\OneDrive\Documents\m.moses-okorieCOS101\week-4\practice_5\target\debug\deps\practice_5.exe: src\main.rs
+
+src\main.rs:
