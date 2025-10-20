6db3b7c340d7af166d41b09b8d22efdaf2a82610
Marvelous Moses-Okorie <marvelous.moses-okorie@pau.edu.ng>
2025-10-20 15:08:13 +0100

    (My eighth commit)

---
+++ week-4/pratice_6/target/debug/deps/pratice_6.d
@@ -0,0 +1,5 @@
+C:\Users\marve\OneDrive\Documents\m.moses-okorieCOS101\week-4\pratice_6\target\debug\deps\pratice_6.d: src\main.rs
+
+C:\Users\marve\OneDrive\Documents\m.moses-okorieCOS101\week-4\pratice_6\target\debug\deps\pratice_6.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/pratice_6/target/debug/pratice_6.d
@@ -0,0 +1,1 @@
+C:\Users\marve\OneDrive\Documents\m.moses-okorieCOS101\week-4\pratice_6\target\debug\pratice_6.exe: C:\Users\marve\OneDrive\Documents\m.moses-okorieCOS101\week-4\pratice_6\src\main.rs
