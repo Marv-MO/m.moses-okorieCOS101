46c6ebbf4a27035a748870f28ca7c8f072d8fd86
Marvelous Moses-Okorie <marvelous.moses-okorie@pau.edu.ng>
2025-11-14 23:16:53 +0100

    (Project commit)

--- week-7/project_1/src/main.rs
+++ week-7/project_1/src/main.rs
@@ -1,25 +1,26 @@
+use std::io;
 use std::f64::consts::PI;
 
-fn area_of_trapezium(h: i32, a: i32, b: i32) {
-    let area = h / 2 * (a + b);
+fn area_of_trapezium(h: f64, a: f64, b: f64) {
+    let area = h / 2.0 * (a + b);
 
     println!("Area of trapezium = {}", area);
 }
 
-fn area_of_rhombus(c: i32, d:i32) {
-    let area = (c * d) / 2;
+fn area_of_rhombus(c: f64, d:f64) {
+    let area = (c * d) / 2.0;
 
     println!("Area of rhomus = {}", area);
 }
 
-fn area_of_parallelogram(e: i32, f: i32) {
+fn area_of_parallelogram(e: f64, f: f64) {
     let area = e * f;
 
     println!("Area of parallelogram = {}", area);
 }
 
-fn area_of_cube(g: i32) {
-    let area = 6 * g * g;
+fn area_of_cube(g: f64) {
+    let area = 6.0 * g * g;
 
     println!("Area of cube = {}", area); 
 }
@@ -30,69 +31,78 @@ fn volume_of_cylinder(j: f64, k: f64) {
     println!("Volume of cylinder = {}", volume);
 }
 
+fn get_1_input(label: &str) -> f64 {
+    println!("Enter {}:", label);
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read");
+    input.trim().parse().expect("Enter a valid number")
+}
+
+fn get_2_inputs(label1: &str, label2: &str) -> (f64, f64) {
+    let x1 = get_1_input(label1);
+    let x2 = get_1_input(label2);
+    (x1,x2) 
+}
+
+fn get_3_inputs(label1: &str, label2: &str, label3: &str) -> (f64, f64, f64) {
+    let x1 = get_1_input(label1);
+    let x2 = get_1_input(label2);
+    let x3 = get_1_input(label3);
+    (x1,x2,x3)
+}
+
 fn main() {
-    let mut input1 = String::new();
-    println!("Enter input for parameter H:");
-    std::io::stdin().read_line(&mut input1).expect("Failed to read input");
-    let h:i32 = input1.trim().parse().expect("Invalid input");
+    loop {
+        println!("\nShape calculator");
+        println!("Select a shape to calculate:");
+        println!("1. Area of trapezium");
+        println!("2. Area of rhombus");
+        println!("3. Area of parallelogram");
+        println!("4. Area of cube");
+        println!("5. Volume of cylinder");
+        println!("0. We are done here");
 
-    let mut input2 = String::new();
-    println!("Enter input for parameter A:");
-    std::io::stdin().read_line(&mut input2).expect("Failed to read input");
-    let a:i32 = input2.trim().parse().expect("Invalid input");
+        let mut choice = String::new();
+        io::stdin().read_line(&mut choice).expect("Invalid input");
+        let choice:u32 = match choice.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number so we can work");
+                continue;
+            }
+        };
+        if choice == 0 {
+            println!("See ya!");
+            break;
+        }
 
-    let mut input3 = String::new();
-    println!("Enter input for parameter B:");
-    std::io::stdin().read_line(&mut input3).expect("Failed to read input");
-    let b:i32 = input3.trim().parse().expect("Invalid input");
-
-    // call add function with argument
-    area_of_trapezium(h, a, b);
-
-    let mut input4 = String::new();
-    println!("Enter input parameter C:");
-    std::io::stdin().read_line(&mut input4).expect("Failed to read input");
-    let c:i32 = input4.trim().parse().expect("Invalid input");
-
-    let mut input5 = String::new();
-    println!("Enter input for parameter D:");
-    std::io::stdin().read_line(&mut input5).expect("Failed to read input");
-    let d:i32 = input5.trim().parse().expect("Invalid input");
-
-    // call add function with argument
-    area_of_rhombus(c, d);
-
-    let mut input6 = String::new();
-    println!("Enter input for parameter E:");
-    std::io::stdin().read_line(&mut input6).expect("Failed to read input");
-    let e:i32 = input6.trim().parse().expect("Invalid input");
-
-    let mut input7 = String::new();
-    println!("Enter input for parameter F:");
-    std::io::stdin().read_line(&mut input7).expect("Failed to read input");
-    let f:i32 = input7.trim().parse().expect("Invalid input");
-
-    // call add function with argument
-    area_of_parallelogram(e, f);
-
-    let mut input8 = String::new();
-    println!("Enter input for parameter G:");
-    std::io::stdin().read_line(&mut input8).expect("Failed to read input");
-    let g:i32 = input8.trim().parse().expect("Invalid input");
-
-    // call add function with argument
-    area_of_cube(g);
-
-    let mut input9 = String::new();
-    println!("Enter input for parameter J:");
-    std::io::stdin().read_line(&mut input9).expect("Failed to read input");
-    let j:f64 = input9.trim().parse().expect("Invalid input");
-
-    let mut input10 = String::new();
-    println!("Enter input for parameter K:");
-    std::io::stdin().read_line(&mut input10).expect("Failed to read input");
-    let k:f64 = input10.trim().parse().expect("Invalid input");
-
-    // call add function with argument
-    volume_of_cylinder(j, k);
+        match choice {
+            1 => {
+                let (h, a, b) = get_3_inputs("h", "a", "b");
+                println!("Area of trapezium");
+                area_of_trapezium(h, a, b);
+            }
+            2 => {
+                let (c, d) = get_2_inputs("c", "d");
+                println!("Area of rhombus");
+                area_of_rhombus(c, d);
+            }
+            3 => {
+                let (e, f) = get_2_inputs("e", "f");
+                println!("Area of parallelogram");
+                area_of_parallelogram(e, f);
+            }
+            4 => {
+                let g = get_1_input("g");
+                println!("Area of cube");
+                area_of_cube(g);
+            }
+            5 => {
+                let (j, k) = get_2_inputs("j", "k");
+                println!("Volume of cylinder");
+                volume_of_cylinder(j, k);
+            }
+            _ => println!("Wrong choice. Pick the correct one"),
+        }
+    }    
 }
