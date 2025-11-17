9963e3a30b2fd62ee8f661be268027b919f59c05
Marvelous Moses-Okorie <marvelous.moses-okorie@pau.edu.ng>
2025-11-17 19:38:02 +0100

    (Practice commit)

---
+++ week-8/practice_12/target/debug/practice_12.d
@@ -0,0 +1,1 @@
+C:\Users\marve\OneDrive\Documents\m.moses-okorieCOS101\week-8\practice_12\target\debug\practice_12.exe: C:\Users\marve\OneDrive\Documents\m.moses-okorieCOS101\week-8\practice_12\src\main.rs
