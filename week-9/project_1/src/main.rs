190df365f2c5b91944bb3ebb58e463b7959a4244
Marvelous Moses-Okorie <marvelous.moses-okorie@pau.edu.ng>
2025-11-24 10:27:41 +0100

    (Project commit)

--- week-9/project_1/src/main.rs
+++ week-9/project_1/src/main.rs
@@ -1,3 +1,19 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    
+   let  brewing_company = "Nigerian Brewery Company";
+
+   let mut file = std::fs::File::create("data.txt").expect("failed to create");
+   file.write_all("    Lager        |     Stout       |     Non-Alcoholic\n 
+                       33 Export    |     Legend      |     Maltina\n      
+                       Desperados   |     Turbo King  |     Amstel Malta\n   
+                       Goldberg     |     Williams    |     Malta Gold\n    
+                       Gulder       |                 |     Fayrouz\n 
+                       Heineken     |                 |            \n  
+                       Star         |                 |            \n"
+       .as_bytes()).expect("write failed");
+   file.write_all(brewing_company.as_bytes()).expect("write failed");
+   println!("\nData written to file. ");
+   
 }
